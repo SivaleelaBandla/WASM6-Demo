6e74db951677cb83b234871486968eb3ea810d58
SivaLeelaBandla <bsivaleela0318@gmail.com>
2022-11-22 09:42:13 +0530

    Commit 3 - WASM6 Sample

--- src/test/java/Demo/SampleTest.java
+++ src/test/java/Demo/SampleTest.java
@@ -7,5 +7,6 @@ public class SampleTest {
  public void sampleTest() {
 	 System.out.println("Hi,This is Sample Test");
 	 System.out.println("Hi,This is Maven Project - User1");
+	 System.out.println("This is Maven Project WASM6 - User1");
  }
 }
